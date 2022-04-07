ee1be1884a80a1a94041fbb2ebbbe1429a7fdef5
Bathtech <bathalx06@gmail.com>
2022-04-07 13:54:23 -0400

    C coding 2

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+printf("size of a char: %d byte(s)\n", sizeof(char));
+printf("size of a int: %d byte(s)\n", sizeof(int));
+printf("size of a long int: %d byte(s)\n", sizeof(long int));
+print("size of a long long int: %d byte(s)\n", sizeof(long long int));
+print("size of a float: %d bytes(s)\n", sizeof(float));
+return (0);
+}
